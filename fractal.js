898cbf3c7b21b0434047a712c1bf5feefcce4bed
Preston Crowe <prestonjcrowe@gmail.com>
2018-04-28 15:59:20 -0700

    hey im dylan

--- fractal.js
+++ fractal.js
@@ -1,18 +1,22 @@
+var frames = 0;
+
 function setup() {
 
 }
-g
+
 function draw() {
   createCanvas(windowHeight,windowHeight);
-  background(29, 27, 28);
+  background(0, 0, 0);
   rectMode(CENTER);
   tri(width / 2, height / 2, width);
+
+  frames++;
 }
 
 function tri(x, y, size) {
-  fill(255);
+  fill(0);
   //triangle(x, y - size / 2, x - size / 2, y + size/ 2, x + size / 2, y + size / 2);
-  sierpinki(width/2, height/2, width, 10);
+  sierpinki(width/2, height/2, width, 8);
 }
 
 function sierpinki(x, y, size, lvl) {
@@ -22,24 +26,29 @@ function sierpinki(x, y, size, lvl) {
 
   else {
     stroke(255, 0, 0);
-    // change color here
-    stroke(random(255), random(255), random(255));
+    colorMode(HSB, 100);
+    stroke(((y / height * 100) + (frames * 10)) % 100, 50, 50);
     noFill();
-    rect(x, y, size, size);
 
-    // dont draw triangles, only "hitboxes"
-    /*
-    fill(0);
-    stroke(0);
-    triangle(x - size * 0.5, y + size/2, x, y + size / 2, x - size * 0.25, y);
-    triangle(x + size * 0.5, y + size/2, x, y + size / 2, x + size * 0.25, y);
-    triangle(x, y - size/2,  x - size *0.25,  y, x + size * 0.25, y);
-    */
-    sierpinki(x - size / 4, y + size / 4, size / 2, lvl - 1);
-    sierpinki(x + size / 4, y + size / 4, size / 2, lvl - 1);
-    sierpinki(x, y - size / 4, size / 2, lvl - 1);
-  }
-  
+    let drawRects = false;
+    let drawTris = true;
+
+    if (drawRects) {
+      rect(x, y, size, size);
+    }
+
+    if (drawTris) {
+      // fill(0);
+      // stroke(0);
+      triangle(x - size * 0.5, y + size/2, x, y + size / 2, x - size * 0.25, y);
+      triangle(x + size * 0.5, y + size/2, x, y + size / 2, x + size * 0.25, y);
+      triangle(x, y - size/2,  x - size *0.25,  y, x + size * 0.25, y);
+
+      sierpinki(x - size / 4, y + size / 4, size / 2, lvl - 1);
+      sierpinki(x + size / 4, y + size / 4, size / 2, lvl - 1);
+      sierpinki(x, y - size / 4, size / 2, lvl - 1);
+    }
+  }  
 }
 
 function carpet(x, y, size, lvl) {
